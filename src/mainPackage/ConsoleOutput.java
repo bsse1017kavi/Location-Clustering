90bfb4a74ff490bdfbef4ecce2b7d7c9d2546b17
bsse1017kavi <bsse1017@iit.du.ac.bd>
2021-09-06 05:02:14 +0600

    Main class refactored

---
+++ src/mainPackage/ConsoleOutput.java
@@ -0,0 +1,27 @@
+package mainPackage;
+
+import locationPackage.LocationClusterer;
+
+import java.util.Scanner;
+
+public class ConsoleOutput
+{
+    public static void main(String[] args) {
+
+        int k;
+
+        Scanner scanner = new Scanner(System.in);
+
+        LocationClusterer locationClusterer = new LocationClusterer();
+
+        locationClusterer.initialize();
+
+        System.out.println("Give the value of k:");
+
+        k = scanner.nextInt();
+
+        locationClusterer.cluster(k);
+
+        locationClusterer.printClusters();
+    }
+}
--- src/mainPackage/Main.java
+++ src/mainPackage/Main.java
@@ -1,27 +1,80 @@
 package mainPackage;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.*;
+import javafx.stage.Stage;
+import locationPackage.Cluster;
+import locationPackage.Coordinate;
 import locationPackage.LocationClusterer;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main
+public class Main extends Application
 {
-    public static void main(String[] args) {
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        stage.setTitle("K means Plotting");
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        final ScatterChart<Number,Number> plot =
+                new ScatterChart<Number,Number>(xAxis,yAxis);
 
-        int k;
-
-        Scanner scanner = new Scanner(System.in);
 
         LocationClusterer locationClusterer = new LocationClusterer();
-
         locationClusterer.initialize();
 
-        System.out.println("Give the value of k:");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the value of k:");
+        int k = scanner.nextInt();
 
-        k = scanner.nextInt();
+        ArrayList<Cluster> clusters = locationClusterer.cluster(k);
 
-        locationClusterer.cluster(k);
+        scanner.close();
 
         locationClusterer.printClusters();
+
+        int i = 0;
+
+        for(Cluster cluster: clusters)
+        {
+
+            XYChart.Series series = new XYChart.Series();
+            series.setName("Cluster " + (i+1));
+
+            for(Coordinate coordinate: cluster.getCoordinates())
+            {
+                series.getData().add(new XYChart.Data(coordinate.getLatitude(),coordinate.getLongitude()));
+            }
+
+            plot.getData().add(series);
+
+            i++;
+        }
+
+        //bc.getData().add(series1);
+
+        Scene scene  = new Scene(plot,800,600);
+
+        Stage stage1 = new Stage();
+        Scene scene1 = BarGraph.centroidBargraph(clusters);
+        stage1.setTitle("Centroid Bar Graph");
+        stage1.setScene(scene1);
+
+        stage.setScene(scene);
+
+        stage1.setX(50);
+        stage1.setY(200);
+        stage1.show();
+
+        stage.setX(1000);
+        stage.setY(200);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
